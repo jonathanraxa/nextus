00c6cdc32a408cbb46d82860467f9cb15e153d2c
Jonathan Raxa <jonathanraxa@gmail.com>
2025-08-10 12:35:46 -0500

    update for skeleton project and write header component

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export const Header = () => (
+    <>
+        <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+        <h1>Welcome!</h1>
+    </>
+)
